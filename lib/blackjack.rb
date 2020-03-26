92068dcca31dcf3e5759671611ddb5ac0e440295
George Chen <georgechen1338@gmail.com>
2020-03-26 05:18:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,5 +66,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(total)
 end
     
